46d9fdc34cab14bad065157027957cfec88f6eaf
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-06-21 03:14:42 -0300

    Implementando o component de servico para consumir o backend mockado pelo json-server

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -1,30 +1,46 @@
+import { Injectable } from '@angular/core'
+import { Http } from '@angular/http'
+
+import { Observable } from 'rxjs'
+import 'rxjs/add/operator/map'
+
 import { IRestaurant } from './restaurant/restaurant.model'
 
+import { MEAT_API } from '../app.api'
+
+@Injectable()
 export class RestaurantsService {
 
-	rests: IRestaurant[] = [
-		{
- 			id: "bread-bakery",
- 			name: "Bread & Bakery",
- 			category: "Bakery",
- 			deliveryEstimate: "25m",
- 			rating: 4.9,
- 			imagePath: "assets/img/restaurants/breadbakery.png"
-		},
-		{
- 			id: "burger-house",
- 			name: "Burger House",
- 			category: "Hamburgers",
- 			deliveryEstimate: "100m",
- 			rating: 3.5,
- 			imagePath: "assets/img/restaurants/burgerhouse.png"
-		}
-	]
+	// rests: IRestaurant[] = [
+	// 	{
+ // 			id: "bread-bakery",
+ // 			name: "Bread & Bakery",
+ // 			category: "Bakery",
+ // 			deliveryEstimate: "25m",
+ // 			rating: 4.9,
+ // 			imagePath: "assets/img/restaurants/breadbakery.png"
+	// 	},
+	// 	{
+ // 			id: "burger-house",
+ // 			name: "Burger House",
+ // 			category: "Hamburgers",
+ // 			deliveryEstimate: "100m",
+ // 			rating: 3.5,
+ // 			imagePath: "assets/img/restaurants/burgerhouse.png"
+	// 	}
+	// ]
 
-	constructor(){}
+	// constructor(){}
 
-	restaurants(): IRestaurant[] {
-		return this.rests
+	// restaurants(): IRestaurant[] {
+	// 	return this.rests
+	// }
+	// http: Http
+
+	constructor(private http: Http){}
+
+	restaurants(): Observable<IRestaurant[]> {
+		return this.http.get(`${MEAT_API}/restaurants`)
+										.map(response => response.json())
 	}
-
 }
