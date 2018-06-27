5d747eb388f181d145752e615184fccd62e56eda
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-06-27 19:40:16 -0300

    48 deixando os dados dinamicos

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -47,4 +47,10 @@ export class RestaurantsService {
 										.map(response => response.json())
 										.catch(ErrorHandler.handleError)
 	}
+
+	restaurantById(id: string): Observable<IRestaurant> {
+		return this.http.get(`${MEAT_API}/restaurants/${id}`)
+										.map(response => response.json())
+										.catch(ErrorHandler.handleError)
+	}
 }
