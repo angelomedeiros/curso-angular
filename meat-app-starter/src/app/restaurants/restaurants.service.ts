51b6478ff986e231039e5f0f23c0088c14824816
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-06-27 21:04:11 -0300

    Usando o pipe nos reviews

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -14,32 +14,6 @@ import {ErrorHandler}  from '../app.error-handler'
 @Injectable()
 export class RestaurantsService {
 
-	// rests: IRestaurant[] = [
-	// 	{
- // 			id: "bread-bakery",
- // 			name: "Bread & Bakery",
- // 			category: "Bakery",
- // 			deliveryEstimate: "25m",
- // 			rating: 4.9,
- // 			imagePath: "assets/img/restaurants/breadbakery.png"
-	// 	},
-	// 	{
- // 			id: "burger-house",
- // 			name: "Burger House",
- // 			category: "Hamburgers",
- // 			deliveryEstimate: "100m",
- // 			rating: 3.5,
- // 			imagePath: "assets/img/restaurants/burgerhouse.png"
-	// 	}
-	// ]
-
-	// constructor(){}
-
-	// restaurants(): IRestaurant[] {
-	// 	return this.rests
-	// }
-	// http: Http
-
 	constructor(private http: Http){}
 
 	restaurants(): Observable<IRestaurant[]> {
@@ -50,6 +24,12 @@ export class RestaurantsService {
 
 	restaurantById(id: string): Observable<IRestaurant> {
 		return this.http.get(`${MEAT_API}/restaurants/${id}`)
+										.map(response => response.json())
+										.catch(ErrorHandler.handleError)
+	}
+
+	reviewsOfRestaurant(id: string): Observable<any> {
+		return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
 										.map(response => response.json())
 										.catch(ErrorHandler.handleError)
 	}
